538da7e67d419296cc10e748593f8666a0196f4b
IvanMarchenkoS00273436 <S00273436@atu.ie>
2025-02-12 11:43:55 +0000

    add bootstrap and implement movie management components

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -8,13 +8,17 @@ export class MovieService {
   constructor() { }
 
   movieList = [
-    { id: 1, title: 'The Shawshank Redemption', year: 1994, director: 'Frank Darabont' },
-    { id: 2, title: 'The Godfather', year: 1972, director: 'Francis Ford Coppola' },
-    { id: 3, title: 'The Godfather: Part II', year: 1974, director: 'Francis Ford Coppola' },
-    { id: 4, title: 'The Dark Knight', year: 2008, director: 'Christopher Nolan' },
+    {  title: 'The Shawshank Redemption', year: "1994", director: 'Frank Darabont' },
+    {  title: 'The Godfather', year: "1972", director: 'Francis Ford Coppola' },
+    {  title: 'The Godfather: Part II', year: "1974", director: 'Francis Ford Coppola' },
+    {  title: 'The Dark Knight', year: "2008", director: 'Christopher Nolan' },
   ];
 
   getMovies() {
     return this.movieList;
+  }
+
+  addMovie(movietitle: string, moviedirector: string, movieyear: string) {
+    this.movieList.push({ title: movietitle, director: moviedirector, year: movieyear });
   }
 }
